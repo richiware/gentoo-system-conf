28c17c058a7305dc209dfabb6c22d04c011c92c5
Ricardo González Moreno <correoricky@gmail.com>
2016-12-05 10:16:22 +0100

    Added dircolors-solarized

---
+++ user-install.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+ln -s "$PWD/thirdparty/dircolors-solarized/dircolors.256dark" ~/.dircolors
+
+#Show message about gnome-terminal-colors-solarized
+echo 'Execute thirdparty/gnome-terminal-colors-solarized/install.sh
