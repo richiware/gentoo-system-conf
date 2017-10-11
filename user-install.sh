b2f1832a011443ca81d188960027067b3643f195
Ricardo González Moreno <correoricky@gmail.com>
2017-10-11 14:20:49 +0200

    User scripts install vim files

--- user-install.sh
+++ user-install.sh
@@ -1,6 +1,19 @@
 #!/bin/sh
 
+#### Check preconditions ####
+
+if [[ -d ~/.vim ]]; then
+    echo "~/.vim directory already exists"
+    exit -1
+fi
+
+#### Installation ####
+
 ln -s "$PWD/thirdparty/dircolors-solarized/dircolors.256dark" ~/.dircolors
 
 #Show message about gnome-terminal-colors-solarized
-echo 'Execute thirdparty/gnome-terminal-colors-solarized/install.sh
+echo "Execute thirdparty/gnome-terminal-colors-solarized/install.sh"
+
+# Install vim files
+ln -s "$PWD/thirdparty/vim-conf" ~/.vim
+mkdir -p ~/.config/nvim && ln -s "$PWD/thirdparty/vim-conf/init.vim" ~/.config/nvim/init.vim
