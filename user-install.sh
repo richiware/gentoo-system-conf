f739bd604ebf445ceddfa7518dd266625bfa4631
Ricardo González Moreno <correoricky@gmail.com>
2017-12-12 14:44:46 +0100

    Added redminetimesync

--- user-install.sh
+++ user-install.sh
@@ -19,7 +19,9 @@ fi
 ln -s "$PWD/thirdparty/dircolors-solarized/dircolors.256dark" ~/.dircolors
 
 #Show message about gnome-terminal-colors-solarized
-echo "Execute thirdparty/gnome-terminal-colors-solarized/install.sh"
+echo "Executing thirdparty/gnome-terminal-colors-solarized/install.sh"
+read -t 10
+sh -c "$PWD/thirdparty/gnome-terminal-colors-solarized/install.sh"
 
 # Install vim files
 ln -s "$PWD/thirdparty/vim-conf" ~/.vim
@@ -33,11 +35,21 @@ ln -s "$PWD/tmux/tmux.desktop" ~/.local/share/applications/tmux.desktop
 ln -s "$PWD/zsh/zshenv" ~/.zshenv
 ln -s "$PWD/zsh/zshrc" ~/.zshrc
 
-# TODO Ejecutar comando taskwarrior para inicializar configuración
+# Execute taskwarrior to create config file
+task list
 
 # Install bugwarrior
 cp "$PWD/bugwarrior/bugwarriorrc" ~/.bugwarriorrc
-echo "Configure ~/.bugwarriorrc"
+echo "Include redmine key in ~/.bugwarriorrc"
+read -t 10
+vim ~/.bugwarriorrc
+
+# Install redminetimesync
+cp "$PWD/thirdparty/redminetimesync/activities.config.tpl" "$PWD/thirdparty/redminetimesync/activities.config"
+cp "$PWD/thirdparty/redminetimesync/redminetimesync.config.tpl" "$PWD/thirdparty/redminetimesync/redminetimesync.config"
+echo "Include redmine key in redminetimesync.config"
+read -t 10
+vim "$PWD/thirdparty/redminetimesync/redminetimesync.config"
 
 # Install taskwhisperer gnome extension
 ln -s "$PWD/thirdparty/taskwhisperer" ~/.local/share/gnome-shell/extensions/taskwhisperer-extension@infinicode.de
